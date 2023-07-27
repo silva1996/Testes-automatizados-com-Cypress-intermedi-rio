b6e53dd980d76921c3931e121cb2736ab9fec0f8
silva1996 <marcosj.antonio3@gmail.com>
2023-07-27 14:54:21 -0300

    envio de arquivos api

---
+++ cypress/support/api_commands.js
@@ -0,0 +1,14 @@
+const accessToken = `Bearer ${Cypress.env('gitlab_access_token')}`
+
+Cypress.Commands.add('api_createProject', project => {
+  cy.request({
+    method: 'POST',
+    url: `/api/v4/projects/`,
+    body: {
+      name: project.name,
+      description: project.description,
+      initialize_with_readme: true
+    },
+    headers: { Authorization: accessToken },
+  })
+})
